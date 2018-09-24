0116b0b3b8fa57f31fc688334812986743eec5a0
Ethan Frigon <ethan.frigon@gmail.com>
2018-09-24 11:57:23 -0700

    added try/catch

--- drill-1.js
+++ drill-1.js
@@ -1,11 +1,19 @@
+'use strict';
 function getYearOfBirth(age){
     return 2018 - age;
 }
 
 function createGreeting(name, age){
+    if(age < 0) {
+        throw new Error("Age can not be negative");
+    }
     return('Hi, my name is ${name} and I\'m ${age} years old.' + 
            'I was born in ${getYearOfBirth(age)}.');
 }
 
+try {
 const greeting1 = createGreeting('Ethan', 29);
 console.log(greeting1);
+} catch (e) {
+    console.log(e);
+}
