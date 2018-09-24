2616367f0ce85f97002ba20830ce288cdd81a42b
Ethan Frigon <ethan.frigon@gmail.com>
2018-09-24 12:14:06 -0700

    added type of errors

--- drill-1.js
+++ drill-1.js
@@ -4,12 +4,18 @@ function getYearOfBirth(age){
     if(age === undefined){
         throw new Error("Arguments not valid");
     }
+    if(typeofage !== "number"){
+        throw new typeError("Not a number");
+    }
     return 2018 - age;
 }
 
 function createGreeting(name, age){
-    if(age === undefined){
+    if(name === undefined){
         throw new Error("Arguments not valid");
+    }
+    if(typeof name !== "string"){
+        throw new typeError("Not a string");
     }
     if(age < 0) {
         throw new Error("Age can not be negative");
